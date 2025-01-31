e08ca0b3e3aac332316e6020274703125e4922b4
paulina koch <pjassakoch@gmail.com>
2025-01-31 09:23:48 -0500

    Initial commit

---
+++ concrete-creations/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => (
+
+
+    <div className="navbar">
+<img src="/images/custom concrete countertops_transparent-.png" alt="logo" />
+<div className="burger-menu">☰</div> 
+
+
+    </div>
+    
+);
+
+export default Navbar;
